8b99947547b821fe6049df94a58091ff442c11e2
JakubCzerwiec <jacob.czerwiec@gmail.com>
2024-10-11 02:42:58 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Mój Maczek on 11/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
